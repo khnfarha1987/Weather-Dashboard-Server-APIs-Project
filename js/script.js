62e5522b03145372b01ebc735f596f94a6649978
Farha Khan <khurshidkhan@Khurshids-MacBook-Pro.local>
2023-05-09 11:48:31 +0100

    Define function to capitalize the first letter of a string.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ var currentDay = moment().format('M/DD/YYYY');
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 var searchHistoryArray = loadSearchHistory();
 //end of global variables//
+
+// Define function to capitalize the first letter of a string...
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+}
